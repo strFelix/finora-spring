bf470b4024b9d26e661c120f6e9e9ea4bfd93920
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2025-10-23 18:58:54 -0300

    feat: enhance global exception handler

--- src/main/java/br/com/strfelix/finora_spring/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/strfelix/finora_spring/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package br.com.strfelix.finora_spring.exception;
 
-import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,6 +24,26 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SecurityException.class)
     public ResponseEntity<String> handleSecurity(SecurityException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(ex.getMessage());
+                .body(ex.getMessage() != null ? ex.getMessage() : "Access unauthorized");
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
+        if (ex.getCause() instanceof JsonProcessingException) {
+            return ResponseEntity.badRequest().body("Error processing JSON: " + ex.getMessage());
+        }
+        return ResponseEntity.internalServerError().body("Internal error: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage() != null ? ex.getMessage() : "Illegal argument.");
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(DataIntegrityViolationException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body("Conflict resource.");
     }
 }
