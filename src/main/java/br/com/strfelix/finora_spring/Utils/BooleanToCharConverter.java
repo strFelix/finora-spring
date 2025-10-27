47f582960e7a320e48e846d77f27acf0bc2902bc
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2025-10-27 20:18:16 -0300

    feat: add Boolean to char converter for database attribute mapping

---
+++ src/main/java/br/com/strfelix/finora_spring/Utils/BooleanToCharConverter.java
@@ -0,0 +1,18 @@
+package br.com.strfelix.finora_spring.Utils;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class BooleanToCharConverter implements AttributeConverter<Boolean, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Boolean value) {
+        return (value != null && value) ? "Y" : "N";
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String value) {
+        return "Y".equalsIgnoreCase(value);
+    }
+}
